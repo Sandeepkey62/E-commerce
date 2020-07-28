ce38591a3c190139d50d39737a56d5a2383967bb
sandeep <sandeepkanoujiya1@gmail.com>
2020-07-28 16:36:23 +0530

    Updates

---
+++ precache-manifest.a54a932476ebee71bdb1524cf4b0a55d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7134b7701942763fc43ddccdfa25a055",
+    "url": "/Sandeepkey62/react-app.git/index.html"
+  },
+  {
+    "revision": "73b207506cce6d3278c3",
+    "url": "/Sandeepkey62/react-app.git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0babbde14e333bc689a2",
+    "url": "/Sandeepkey62/react-app.git/static/js/2.8aaac6e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Sandeepkey62/react-app.git/static/js/2.8aaac6e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73b207506cce6d3278c3",
+    "url": "/Sandeepkey62/react-app.git/static/js/main.1c63678c.chunk.js"
+  },
+  {
+    "revision": "692916611a9c60b058db",
+    "url": "/Sandeepkey62/react-app.git/static/js/runtime-main.9025cc95.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Sandeepkey62/react-app.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
